8e89b2358f3fc9435bcdc3cd9369c218bec5b58c
Yuriy Miroshnyk <y.miroshnyk@gmail.com>
2017-02-02 20:21:44 +0200

    Initial distrib.

---
+++ distrib-unity/Assets/neutrinoparticles/renderer/NeutrinoRenderBuffer.cs
@@ -0,0 +1,151 @@
+﻿//#define CACHE_TESTING
+using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace Neutrino.Unity3D
+{
+	public class NeutrinoRenderBuffer : MonoBehaviour, Neutrino.RenderBuffer
+	{
+		#region Fields
+		[Tooltip("Activate the mechanism that will free memory used for indices cache. Might add some noticeable CPU overhead on complex effects")]
+		public bool autoReleaseMemory = true;
+		private List<Vector3> vertices;
+		private List<Vector2> uv;
+		private int[] allIndices;
+		private List<int> indices;
+		private List<Color> colors;
+
+		private int vertexCount;
+		private Mesh mesh;
+
+		private RenderCall[] renderCalls;
+		private int renderCallsCount;
+		private Bounds bounds = new Bounds();
+		#endregion
+
+		#region Properties
+		public Mesh Mesh
+		{
+			get
+			{
+				return mesh;
+			}
+		}
+
+		public RenderCall[] RenderCalls
+		{
+			get
+			{
+				return renderCalls;
+			}
+		}
+
+		public int RenderCallsCount
+		{
+			get
+			{
+				return renderCallsCount;
+			}
+		}
+		#endregion
+
+		#region Methods
+		public void cleanup()
+		{
+			renderCallsCount = 0;
+			vertexCount = 0;
+
+			vertices.Clear();
+			colors.Clear();
+			uv.Clear();
+		}
+
+		public void initialize(uint maxNumVertices, uint[] texChannels, ushort[] indices, uint maxNumRenderCalls)
+		{
+			renderCallsCount = 0;
+			vertexCount = 0;
+
+			mesh = new Mesh();
+			vertices = new List<Vector3>((int)maxNumVertices);
+			uv = new List<Vector2>((int)maxNumVertices);
+			colors = new List<Color>((int)maxNumVertices);
+
+			mesh.MarkDynamic();
+			gameObject.AddComponent<MeshFilter>().mesh = mesh;
+
+			this.allIndices = Array.ConvertAll(indices, (ushort s) => { return (int)s; });
+			this.indices = new List<int>(indices.Length);
+
+			renderCalls = new RenderCall[maxNumRenderCalls];
+		}
+
+		public void pushRenderCall(ref RenderCall rc)
+		{
+			renderCalls[renderCallsCount++] = rc;
+		}
+
+		public void pushVertex(ref RenderVertex v)
+		{
+			Vector3 v3;
+			v3.x = v.position_.x;
+			v3.y = v.position_.y;
+			v3.z = v.position_.z;
+			vertices.Add(v3);
+
+			Vector2 v2;
+			v2.x = v.texCoords_[0][0];
+			v2.y = v.texCoords_[0][1];
+			uv.Add(v2);
+
+			Color c;
+			c.a = ((float)((v.color_ & 0xff000000) >> 24)) / 255f;
+			c.r = ((float)(v.color_ & 0x000000ff)) / 255f;
+			c.g = ((float)((v.color_ & 0x0000ff00) >> 8)) / 255f; 
+			c.b = ((float)((v.color_ & 0x00ff0000) >> 16)) / 255f;
+			colors.Add(c);
+
+			++vertexCount;
+		}
+
+		public void updateIndices(int startingIndex, int indexCount, int subMeshId)
+		{
+			this.indices.Clear();
+			int endIndex = startingIndex + indexCount;
+			for (int i = startingIndex; i < endIndex; ++i)
+				this.indices.Add(this.allIndices[i]);
+
+			mesh.SetTriangles(this.indices, subMeshId);
+
+			//seems like those calls are not required
+			//mesh.RecalculateBounds();
+			//mesh.RecalculateNormals(); //expensive call - uncomment only if needed
+		}
+
+		public void update(int subMeshCount)
+		{
+			if (vertexCount == 0)
+				return;
+
+			mesh.Clear(true);
+
+			mesh.SetVertices(vertices);
+			mesh.SetColors(colors);
+			mesh.SetUVs(0, uv);
+			mesh.subMeshCount = subMeshCount;
+
+
+#if UNITY_EDITOR
+			if (mesh.bounds.center != Vector3.zero)
+			{
+				bounds.SetMinMax(mesh.bounds.min, mesh.bounds.max);
+				bounds.center = Vector3.zero;
+				mesh.bounds = bounds;
+			}
+#endif
+		}
+	#endregion
+	}
+}
---
+++ distrib-unity/Assets/neutrinoparticles/renderer/NeutrinoRenderer.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Neutrino.Unity3D
+{
+	//[ExecuteInEditMode] <--this feature is not available at the moment
+	[RequireComponent(typeof(NeutrinoMaterials))]
+	[RequireComponent(typeof(NeutrinoRenderBuffer))]
+	public class NeutrinoRenderer : MonoBehaviour
+	{
+		#region Fields
+		public bool simulateInWorldSpace = false;
+		new Neutrino.System particleSystem;
+		Neutrino.System.Impl particleSytemImpl;
+		Neutrino.NMath.vec3 pos;
+		private NMath.quat rot;
+
+		NeutrinoMaterials nmaterials;
+		NeutrinoRenderBuffer renderBuffer;
+		//int layerMask = 1 << 1; //layer mask is: TransparentFX
+		#endregion
+
+		#region Methods
+		private void RenderParticleSystem(CommandBuffer buf)
+		{
+			//TODO: to support multiple cameras rendering - will probably require to switch rendering routine to CommandBuffer
+			Transform camTrans = Camera.main.transform;
+
+			if (simulateInWorldSpace)
+			{
+				particleSystem.updateRenderBuffer(
+					Neutrino.NMath.vec3_(camTrans.right.x, camTrans.right.y, camTrans.right.z),
+					Neutrino.NMath.vec3_(camTrans.up.x, camTrans.up.y, camTrans.up.z),
+					Neutrino.NMath.vec3_(camTrans.forward.x, camTrans.forward.y, camTrans.forward.z), 
+					pos, rot);
+			}
+			else
+			{
+				//Neutrino.NMath.setv3(out pos, 0f, 0f, 0f);
+				particleSystem.updateRenderBuffer(
+					Neutrino.NMath.vec3_(camTrans.right.x, camTrans.right.y, camTrans.right.z),
+					Neutrino.NMath.vec3_(camTrans.up.x, camTrans.up.y, camTrans.up.z),
+					Neutrino.NMath.vec3_(camTrans.forward.x, camTrans.forward.y, camTrans.forward.z), pos);
+            }
+
+			renderBuffer.update(particleSytemImpl.renderStyles().Length);
+
+			for (int i = 0; i < renderBuffer.RenderCallsCount; ++i)
+			{
+				switch (particleSytemImpl.materials()[particleSytemImpl.renderStyles()[renderBuffer.RenderCalls[i].renderStyleIndex_].material_])
+				{
+					case Neutrino.RenderMaterial.Add:
+						nmaterials.switchToAdd(renderBuffer.RenderCalls[i].renderStyleIndex_);
+						break;
+					case Neutrino.RenderMaterial.Multiply:
+						nmaterials.switchToMultiply(renderBuffer.RenderCalls[i].renderStyleIndex_);
+						break;
+					default:
+						nmaterials.switchToNormal(renderBuffer.RenderCalls[i].renderStyleIndex_);
+						break;
+				}
+
+				renderBuffer.updateIndices(
+					renderBuffer.RenderCalls[i].startIndex_,
+					renderBuffer.RenderCalls[i].numIndices_,
+					renderBuffer.RenderCalls[i].renderStyleIndex_);
+			}
+		}
+
+		// Use this for initialization
+		void Start()
+		{
+			if (simulateInWorldSpace)
+				Neutrino.NMath.setv3(out pos, gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+			else
+			{
+				Neutrino.NMath.setv3(out pos, 0f, 0f, 0f);
+				Neutrino.NMath.setq(out rot, 0f, 0f, 0f, 0f);
+			}
+
+			Component[] components = GetComponents<MonoBehaviour>();
+			foreach (Component c in components)
+			{
+				if (c is Neutrino.System.Impl)
+					particleSytemImpl = c as Neutrino.System.Impl;
+			}
+
+			renderBuffer = GetComponent<NeutrinoRenderBuffer>();
+			particleSystem = new Neutrino.System(particleSytemImpl, renderBuffer, pos);
+
+			nmaterials = GetComponent<NeutrinoMaterials>();
+			nmaterials.LoadTextures(particleSytemImpl.textures(), particleSytemImpl.renderStyles(), new int[] { 0 }); //simplification while texture channels are unsupported
+
+			MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
+			mr.sharedMaterials = nmaterials.materials;
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (simulateInWorldSpace)
+			{
+				//Workaround for flipped moving effects
+				//Mathf.Sign(transform.localScale.x) * transform.position.x,
+				//	Mathf.Sign(transform.localScale.y) * transform.position.y,
+				//	Mathf.Sign(transform.localScale.z) * transform.position.z);
+				Neutrino.NMath.setv3(out pos, transform.position.x, transform.position.y, transform.position.z);
+				Neutrino.NMath.setq(out rot, transform.rotation.w, transform.rotation.x, transform.rotation.y, transform.rotation.z);
+			}
+
+			particleSystem.update(Time.smoothDeltaTime, pos);
+			RenderParticleSystem(null);
+		}
+		#endregion
+
+		#region DesignTime
+		public float gizmoSize = 10;
+		public void OnDrawGizmos()
+		{
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawWireSphere(transform.position, gizmoSize);
+		}
+
+		public void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(transform.position, gizmoSize);
+		}
+		#endregion
+	}
+}
