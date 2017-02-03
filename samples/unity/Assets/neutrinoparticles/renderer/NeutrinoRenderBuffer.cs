42a240b678b04c12bcbbeaf822decf9cf3ae23e8
Yuriy Miroshnyk <y.miroshnyk@gmail.com>
2017-02-03 16:06:51 +0200

    Refactoring and polishing. !simulateInWorldSpace fix.

--- samples/unity/Assets/neutrinoparticles/renderer/NeutrinoRenderBuffer.cs
+++ samples/unity/Assets/neutrinoparticles/renderer/NeutrinoRenderBuffer.cs
@@ -12,20 +12,22 @@ namespace Neutrino.Unity3D
 	public class NeutrinoRenderBuffer : MonoBehaviour, Neutrino.RenderBuffer
 	{
 		#region Fields
-		private Mesh mesh = null;
-		private MeshFilter meshFilter = null;
+		private Mesh mesh_;
 
-		private List<Vector3> vertices;
-		private List<Vector2> uv;
-		private int[] allIndices;
-		private List<int> indices;
-		private List<Color> colors;
+		private List<Vector3> vertices_;
+		private List<Vector2> uv_;
+		private int[] allIndices_;
+		private List<int> indices_;
+		private List<Color> colors_;
 
-		private int vertexCount;
+		private int vertexCount_;
 
-		private RenderCall[] renderCalls;
-		private int renderCallsCount;
-		private Bounds bounds = new Bounds();
+		private RenderCall[] renderCalls_;
+		private int renderCallsCount_;
+
+#if UNITY_EDITOR
+		private Bounds bounds_ = new Bounds();
+#endif
 		#endregion
 
 		#region Properties
@@ -33,7 +35,7 @@ namespace Neutrino.Unity3D
 		{
 			get
 			{
-				return mesh;
+				return mesh_;
 			}
 		}
 
@@ -41,7 +43,7 @@ namespace Neutrino.Unity3D
 		{
 			get
 			{
-				return renderCalls;
+				return renderCalls_;
 			}
 		}
 
@@ -49,7 +51,7 @@ namespace Neutrino.Unity3D
 		{
 			get
 			{
-				return renderCallsCount;
+				return renderCallsCount_;
 			}
 		}
 		#endregion
@@ -58,39 +60,39 @@ namespace Neutrino.Unity3D
 
 		void Start()
 		{
-			mesh = new Mesh();
-			mesh.MarkDynamic();
-			gameObject.GetComponent<MeshFilter>().mesh = mesh;
+			mesh_ = new Mesh();
+			mesh_.MarkDynamic();
+			gameObject.GetComponent<MeshFilter>().mesh = mesh_;
 		}
 
 		public void initialize(uint maxNumVertices, uint[] texChannels, ushort[] indices, uint maxNumRenderCalls)
 		{
-			renderCallsCount = 0;
-			vertexCount = 0;
+			renderCallsCount_ = 0;
+			vertexCount_ = 0;
 
-			vertices = new List<Vector3>((int)maxNumVertices);
-			uv = new List<Vector2>((int)maxNumVertices);
-			colors = new List<Color>((int)maxNumVertices);
+			vertices_ = new List<Vector3>((int)maxNumVertices);
+			uv_ = new List<Vector2>((int)maxNumVertices);
+			colors_ = new List<Color>((int)maxNumVertices);
 
-			this.allIndices = Array.ConvertAll(indices, (ushort s) => { return (int)s; });
-			this.indices = new List<int>(indices.Length);
+			this.allIndices_ = Array.ConvertAll(indices, (ushort s) => { return (int)s; });
+			this.indices_ = new List<int>(indices.Length);
 
-			renderCalls = new RenderCall[maxNumRenderCalls];
+			renderCalls_ = new RenderCall[maxNumRenderCalls];
 		}
 
 		public void cleanup()
 		{
-			renderCallsCount = 0;
-			vertexCount = 0;
+			renderCallsCount_ = 0;
+			vertexCount_ = 0;
 
-			vertices.Clear();
-			colors.Clear();
-			uv.Clear();
+			vertices_.Clear();
+			colors_.Clear();
+			uv_.Clear();
 		}
 
 		public void pushRenderCall(ref RenderCall rc)
 		{
-			renderCalls[renderCallsCount++] = rc;
+			renderCalls_[renderCallsCount_++] = rc;
 		}
 
 		public void pushVertex(ref RenderVertex v)
@@ -99,21 +101,21 @@ namespace Neutrino.Unity3D
 			v3.x = v.position_.x;
 			v3.y = v.position_.y;
 			v3.z = v.position_.z;
-			vertices.Add(v3);
+			vertices_.Add(v3);
 
 			Vector2 v2;
 			v2.x = v.texCoords_[0][0];
 			v2.y = v.texCoords_[0][1];
-			uv.Add(v2);
+			uv_.Add(v2);
 
 			Color c;
 			c.a = ((float)((v.color_ & 0xff000000) >> 24)) / 255f;
 			c.r = ((float)(v.color_ & 0x000000ff)) / 255f;
 			c.g = ((float)((v.color_ & 0x0000ff00) >> 8)) / 255f; 
 			c.b = ((float)((v.color_ & 0x00ff0000) >> 16)) / 255f;
-			colors.Add(c);
+			colors_.Add(c);
 
-			++vertexCount;
+			++vertexCount_;
 		}
 
 			//seems like those calls are not required
@@ -122,36 +124,36 @@ namespace Neutrino.Unity3D
 
 		public void updateMesh()
 		{
-			if (vertexCount == 0)
+			if (vertexCount_ == 0)
 				return;
 
-			mesh.Clear(true);
+			mesh_.Clear(true);
 
-			mesh.SetVertices(vertices);
-			mesh.SetColors(colors);
-			mesh.SetUVs(0, uv);
+			mesh_.SetVertices(vertices_);
+			mesh_.SetColors(colors_);
+			mesh_.SetUVs(0, uv_);
 
-			mesh.subMeshCount = renderCallsCount;
+			mesh_.subMeshCount = renderCallsCount_;
 
-			for (uint renderCallIndex = 0; renderCallIndex < renderCallsCount; ++renderCallIndex)
+			for (uint renderCallIndex = 0; renderCallIndex < renderCallsCount_; ++renderCallIndex)
 			{
-				RenderCall rc = renderCalls[renderCallIndex];
+				RenderCall rc = renderCalls_[renderCallIndex];
 
-				this.indices.Clear();
+				this.indices_.Clear();
 				int endIndex = rc.startIndex_ + rc.numIndices_;
 				for (int i = rc.startIndex_; i < endIndex; ++i)
-					this.indices.Add(this.allIndices[i]);
+					this.indices_.Add(this.allIndices_[i]);
 
-				mesh.SetTriangles(this.indices, (int)renderCallIndex);
+				mesh_.SetTriangles(this.indices_, (int)renderCallIndex);
 			}
 
 
 #if UNITY_EDITOR
-			if (mesh.bounds.center != Vector3.zero)
+			if (mesh_.bounds.center != Vector3.zero)
 			{
-				bounds.SetMinMax(mesh.bounds.min, mesh.bounds.max);
-				bounds.center = Vector3.zero;
-				mesh.bounds = bounds;
+				bounds_.SetMinMax(mesh_.bounds.min, mesh_.bounds.max);
+				bounds_.center = Vector3.zero;
+				mesh_.bounds = bounds_;
 			}
 #endif
 		}
--- samples/unity/Assets/neutrinoparticles/renderer/NeutrinoRenderer.cs
+++ samples/unity/Assets/neutrinoparticles/renderer/NeutrinoRenderer.cs
@@ -14,72 +14,65 @@ namespace Neutrino.Unity3D
 	{
 		#region Fields
 		public bool simulateInWorldSpace = true;
-		new Neutrino.System particleSystem;
-		Neutrino.System.Impl particleSytemImpl;
-		Neutrino.NMath.vec3 pos;
-		private NMath.quat rot;
 
-		Material[] materials;
+		private Neutrino.System.Impl neutrinoSystemImpl_;
+		private Neutrino.System neutrinoSystem_;
 
-		private Shader standard;
-		private Shader multiply;
-		private Shader additive;
+		private Material[] materials_;
+		private Shader shaderNormal_;
+		private Shader shaderAdd_;
+		private Shader shaderMultiply_;
 
-		NeutrinoRenderBuffer renderBuffer;
+		NeutrinoRenderBuffer renderBuffer_;
 		#endregion
 
 		#region Methods
 
 		void Awake()
 		{
-			standard = Shader.Find("Neutrino/Neutrino - AlphaBlended");
-			multiply = Shader.Find("Neutrino/Neutrino - Multiply");
-			additive = Shader.Find("Neutrino/Neutrino - Additive");
+			shaderNormal_ = Shader.Find("Neutrino/Normal");
+			shaderAdd_ = Shader.Find("Neutrino/Add");
+			shaderMultiply_ = Shader.Find("Neutrino/Multiply");
 		}
 
 		void Start()
 		{
-			if (simulateInWorldSpace)
-				Neutrino.NMath.setv3(out pos, gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
-			else
-			{
-				Neutrino.NMath.setv3(out pos, 0f, 0f, 0f);
-				Neutrino.NMath.setq(out rot, 0f, 0f, 0f, 0f);
-			}
-
 			Component[] components = GetComponents<MonoBehaviour>();
 			foreach (Component c in components)
 			{
 				if (c is Neutrino.System.Impl)
-					particleSytemImpl = c as Neutrino.System.Impl;
+					neutrinoSystemImpl_ = c as Neutrino.System.Impl;
 			}
 
-			renderBuffer = GetComponent<NeutrinoRenderBuffer>();
-			particleSystem = new Neutrino.System(particleSytemImpl, renderBuffer, pos);
+			renderBuffer_ = GetComponent<NeutrinoRenderBuffer>();
+			neutrinoSystem_ = new Neutrino.System(neutrinoSystemImpl_, renderBuffer_,
+				simulateInWorldSpace ? 
+					Neutrino.NMath.vec3_(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z) :
+					Neutrino.NMath.vec3_(0, 0, 0));
 
 			// preparing materials
 			{
-				string[] textures = particleSytemImpl.textures();
-				RenderStyle[] renderStyles = particleSytemImpl.renderStyles();
+				string[] textures = neutrinoSystemImpl_.textures();
+				RenderStyle[] renderStyles = neutrinoSystemImpl_.renderStyles();
 
-				materials = new Material[renderStyles.Length];
+				materials_ = new Material[renderStyles.Length];
 
 				for (int i = 0; i < renderStyles.Length; ++i)
 				{
 					Material material;
 
-					switch (particleSytemImpl.materials()[renderStyles[i].material_])
+					switch (neutrinoSystemImpl_.materials()[renderStyles[i].material_])
 					{
 						default:
-							material = new Material(standard);
+							material = new Material(shaderNormal_);
 							break;
 
 						case Neutrino.RenderMaterial.Add:
-							material = new Material(additive);
+							material = new Material(shaderAdd_);
 							break;
 
 						case Neutrino.RenderMaterial.Multiply:
-							material = new Material(multiply);
+							material = new Material(shaderMultiply_);
 							break;
 					}
 
@@ -103,53 +96,49 @@ namespace Neutrino.Unity3D
 					material.name = filename;				
 					material.SetTexture("_MainTex", texture);
 
-					materials[i] = material;
+					materials_[i] = material;
 				}
 			}
 		}
 
-		// Update is called once per frame
 		void Update()
 		{
-			if (simulateInWorldSpace)
-			{
-				//Workaround for flipped moving effects
-				//Mathf.Sign(transform.localScale.x) * transform.position.x,
-				//	Mathf.Sign(transform.localScale.y) * transform.position.y,
-				//	Mathf.Sign(transform.localScale.z) * transform.position.z);
-				Neutrino.NMath.setv3(out pos, transform.position.x, transform.position.y, transform.position.z);
-				Neutrino.NMath.setq(out rot, transform.rotation.w, transform.rotation.x, transform.rotation.y, transform.rotation.z);
-			}
-
-			particleSystem.update(Time.deltaTime, pos);
-
-			//TODO: to support multiple cameras rendering - will probably require to switch rendering routine to CommandBuffer
 			Transform camTrans = Camera.main.transform;
 
+			Neutrino.NMath.vec3 cameraRight =
+				Neutrino.NMath.vec3_(camTrans.right.x, camTrans.right.y, camTrans.right.z);
+			Neutrino.NMath.vec3 cameraUp = 
+				Neutrino.NMath.vec3_(camTrans.up.x, camTrans.up.y, camTrans.up.z);
+			Neutrino.NMath.vec3 cameraDir =
+				Neutrino.NMath.vec3_(camTrans.forward.x, camTrans.forward.y, camTrans.forward.z);
+
+			Neutrino.NMath.quat rot = Neutrino.NMath.quat_(transform.rotation.w, transform.rotation.x, transform.rotation.y, transform.rotation.z);
+
 			if (simulateInWorldSpace)
 			{
-				particleSystem.updateRenderBuffer(
-					Neutrino.NMath.vec3_(camTrans.right.x, camTrans.right.y, camTrans.right.z),
-					Neutrino.NMath.vec3_(camTrans.up.x, camTrans.up.y, camTrans.up.z),
-					Neutrino.NMath.vec3_(camTrans.forward.x, camTrans.forward.y, camTrans.forward.z),
-					pos, rot);
+				Neutrino.NMath.vec3 pos = Neutrino.NMath.vec3_(transform.position.x, transform.position.y, transform.position.z);
+
+				neutrinoSystem_.update(Time.deltaTime, pos);
+				neutrinoSystem_.updateRenderBuffer(cameraRight, cameraUp, cameraDir, pos, rot);
 			}
 			else
 			{
-				//Neutrino.NMath.setv3(out pos, 0f, 0f, 0f);
-				particleSystem.updateRenderBuffer(
-					Neutrino.NMath.vec3_(camTrans.right.x, camTrans.right.y, camTrans.right.z),
-					Neutrino.NMath.vec3_(camTrans.up.x, camTrans.up.y, camTrans.up.z),
-					Neutrino.NMath.vec3_(camTrans.forward.x, camTrans.forward.y, camTrans.forward.z), pos);
+				Neutrino.NMath.quat invRot = Neutrino.NMath.inverseq_(rot);
+				Neutrino.NMath.vec3 localCameraRight = Neutrino.NMath.applyv3quat_(cameraRight, invRot);
+				Neutrino.NMath.vec3 localCameraUp = Neutrino.NMath.applyv3quat_(cameraUp, invRot);
+				Neutrino.NMath.vec3 localCameraDir = Neutrino.NMath.applyv3quat_(cameraDir, invRot);
+
+				neutrinoSystem_.update(Time.deltaTime, Neutrino.NMath.vec3_(0, 0, 0));
+				neutrinoSystem_.updateRenderBuffer(localCameraRight, localCameraUp, localCameraDir, null, null);
 			}
 
-			renderBuffer.updateMesh();
+			renderBuffer_.updateMesh();
 
-			Material[] subMeshMaterials = new Material[renderBuffer.RenderCallsCount];
+			Material[] subMeshMaterials = new Material[renderBuffer_.RenderCallsCount];
 
-			for (uint renderCallIndex = 0; renderCallIndex < renderBuffer.RenderCallsCount; ++renderCallIndex)
+			for (uint renderCallIndex = 0; renderCallIndex < renderBuffer_.RenderCallsCount; ++renderCallIndex)
 			{
-				subMeshMaterials[renderCallIndex] = materials[renderBuffer.RenderCalls[renderCallIndex].renderStyleIndex_];
+				subMeshMaterials[renderCallIndex] = materials_[renderBuffer_.RenderCalls[renderCallIndex].renderStyleIndex_];
 			}
 
 			MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
